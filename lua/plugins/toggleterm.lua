0c26367b7862fb2f7be679d3d104db462ca2c233
Rigidity <me@rigidnetwork.com>
2024-02-24 22:33:07 -0500

    Toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,9 +6,18 @@ return {
     vim.api.nvim_command("autocmd TermOpen * nnoremap <buffer><LeftRelease> <LeftRelease>i")
     vim.api.nvim_command("augroup end")
 
+    -- vim.keymap.set("i", "<C-S-s>", "<cmd>:ToggleTerm<cr>")
+
     require("toggleterm").setup({
+      size = 12,
+      open_mapping = [[<c-s>]],
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 1,
       start_in_insert = true,
-      persist_mode = true,
+      persist_size = true,
+      close_on_exit = true,
+      direction = "horizontal",
     })
   end,
 }
